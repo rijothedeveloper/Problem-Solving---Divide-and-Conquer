71d3148fdf9704d3e748a70c744c03bb507300f8
Rijo George <rijogeorge@rijos-mbp.lan>
2022-04-07 11:08:10 -0700

    sorted frequence done and all done

--- sorted-frequency.js
+++ sorted-frequency.js
@@ -1,5 +1,35 @@
-function sortedFrequency() {
-
+function sortedFrequency(arr, no) {
+  const firstIndex = findFirstOccurance(arr, no);
+  if (firstIndex === -1) return -1;
+  const lastIndex = findLastOccurance(arr, no);
+  return lastIndex - firstIndex + 1;
 }
 
-module.exports = sortedFrequency
+function findFirstOccurance(arr, no, low = 0, high = arr.length - 1) {
+  if (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (arr[mid] === no && (mid === 0 || arr[mid - 1] !== no)) {
+      return mid;
+    } else if (arr[mid] >= no) {
+      return findFirstOccurance(arr, no, low, mid - 1);
+    }
+    return findFirstOccurance(arr, no, mid + 1, high);
+  }
+  return -1;
+}
+
+function findLastOccurance(arr, no, low = 0, high = arr.length - 1) {
+  if (low <= high) {
+    const mid = Math.floor((low + high) / 2);
+    if (arr[mid] === no && (mid === arr.length - 1 || arr[mid + 1] !== no)) {
+      return mid;
+    } else if (arr[mid] <= no) {
+      return findLastOccurance(arr, no, mid + 1, high);
+    } else {
+      return findLastOccurance(arr, no, low, mid - 1);
+    }
+  }
+  return -1;
+}
+
+module.exports = sortedFrequency;
